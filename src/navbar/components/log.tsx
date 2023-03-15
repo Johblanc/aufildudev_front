3abf460c32c5fe20c878e7d4819b21154f721da5
TB <terry.boeuf@hotmail.fr>
2023-03-15 12:46:54 +0100

    add:login Tostify terminer

--- src/navbar/components/log.tsx
+++ src/navbar/components/log.tsx
@@ -1,6 +1,8 @@
 import { useContext, useRef, useState } from 'react';
 import { BASE_URL } from '../../constant/url';
 import { UserContext } from '../../context/UserContext';
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 export function LoginForm(/* props: {
     setUser: React.Dispatch<React.SetStateAction<TUser>>;
@@ -10,6 +12,19 @@ export function LoginForm(/* props: {
     const passwordRef = useRef<HTMLInputElement>(null);
     const { setUser } = useContext(UserContext);
     const userData = useContext(UserContext);
+    const notify = (msg: string,) => toast(msg,
+        {
+            position: "bottom-right",
+            autoClose: 3000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: false,
+            draggable: false,
+            progress: undefined,
+            theme: "light",
+        });
+    ;
+
 
 
 
@@ -31,6 +46,7 @@ export function LoginForm(/* props: {
                 .then((response) => response.json())
 
                 .then((data) => {
+                    console.log(data);
                     if (data.data) {
                         setMessage([data.message]);
                         pseudoRef.current!.value = '';
@@ -39,88 +55,96 @@ export function LoginForm(/* props: {
                         setTimeout(() => {
                             document.getElementById("close")?.click();
                         }, 1500);
-
+                        notify(data.message)
+                        
                     } else {
                         if (typeof data.message === 'string') {
-                            setMessage([data.message]);
+                            notify(data.message);
                         } else {
-                            setMessage(data.message);
+                            notify(data.message[0]);
+                            data.message.forEach((element: string) => {
+                                notify(element)
+                            });
                         }
                     }
                 });
         } else {
-            alert('informations manquantes');
+            notify('informations manquantes');
         }
     };
 
-    const displayMessages = message.map((item, i) => <p className='align-item-center' key={i}>{item}</p>);
+
 
 
     return (
-        <div className="modal fade" aria-labelledby='loginModal' id="loginModal" tabIndex={-1}>
-            <div className="modal-dialog modal-dialog-centered ">
-                <div className="modal-content login-color ">
-                    <div className="modal-header">
-                        <h5 className="modal-title">LOGIN</h5>
-                        <button
-                            type="button"
-                            className="btn-close green-close"
-                            data-bs-dismiss="modal"
-                            aria-label="Close"
-                            id="close"
+        <>
+            <div className="modal fade" aria-labelledby='loginModal' id="loginModal" tabIndex={-1}>
+                <div className="modal-dialog modal-dialog-centered ">
+                    <div className="modal-content login-color ">
+                        <div className="modal-header">
+                            <h5 className="modal-title">LOGIN</h5>
+                            <button
+                                type="button"
+                                className="btn-close green-close"
+                                data-bs-dismiss="modal"
+                                aria-label="Close"
+                                id="close"
 
-                        ></button>
-                    </div>
-                    <div className="modal-body login-color">
-                        <label
-                            htmlFor="exampleFormControlInput1"
-                            className="form-label"
-                        >
-                            Identifiant
-                        </label>
-                        <input
-                            ref={pseudoRef}
-                            type="pseudo"
-                            className="form-control"
-                            id="inputEmail"
-                            placeholder="Pseudo"
-                        ></input>
-                        <br></br>
-                        <label htmlFor="inputPassword" className="form-label">
-                            Password
-                        </label>
-                        <input
-                            ref={passwordRef}
-                            type="password"
-                            className="form-control"
-                            id="inputPassword"
-                            placeholder="password"
-                        ></input>
-                    </div>
+                            ></button>
+                        </div>
+                        <div className="modal-body login-color">
+                            <label
+                                htmlFor="exampleFormControlInput1"
+                                className="form-label"
+                            >
+                                Identifiant
+                            </label>
+                            <input
+                                ref={pseudoRef}
+                                type="pseudo"
+                                className="form-control"
+                                id="inputEmail"
+                                placeholder="Pseudo"
+                            ></input>
+                            <br></br>
+                            <label htmlFor="inputPassword" className="form-label">
+                                Password
+                            </label>
+                            <input
+                                ref={passwordRef}
+                                type="password"
+                                className="form-control"
+                                id="inputPassword"
+                                placeholder="password"
+                            ></input>
+                        </div>
 
-                    <div className="modal-footer d-flex justify-content-center">
-                        {userData.user.access_lvl < 1 ? (
-                            <>
-                                <button
-                                    onClick={submitHandler}
-                                    type="button"
-                                    className="btn btn-green"
-
-                                >
-                                    {' '}
-                                    Connexion
-                                </button></>
-
-                        ) : (
-                            ''
-                        )}
-                        <div className=''>
-                            {displayMessages}
+                        <div className="modal-footer d-flex justify-content-center">
+                            {userData.user.access_lvl < 1 ? (
+                                <>
+                                    <button
+                                        onClick={submitHandler}
+                                        type="button"
+                                        className="btn btn-green"
+                                    >
+                                        {' '}
+                                        Connexion
+                                    </button></>
+                            ) : (
+                                ''
+                            )}
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
+
+            <div>
+
+
+                <ToastContainer />
+
+            </div>
+        </>
     );
 }
 
