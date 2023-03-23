3701907c5aca96816d3f828bc3531812cb872eda
Johblanc <79146321+Johblanc@users.noreply.github.com>
2023-03-23 08:25:11 +0100

    Style : footer

--- src/footer/components/footer.tsx
+++ src/footer/components/footer.tsx
@@ -4,31 +4,12 @@ export default function Footer() {
 
 
     return (
-        <>
+        <footer >
+          <div className="footer-fill-h bg-dark text-light">
+            FOOTER
+          </div>
 
-            <svg className="bg-img-footer"
-                width="100vw"
-                
-                
-
-                preserveAspectRatio="true"
-                viewBox="0 0 100 7"
-                version="1.1"
-                xmlns="http://www.w3.org/2000/svg"
-
-            >
-                <path
-                    style={{ fill: '#D9D9D9', strokeWidth: 0.836684 }}
-                    d="m -16.499998,10.500001 c 11.5000001,-3.0000003 26.5,-4.0000002 31.5,-4.0000002 4,0 14.999999,0.9999999 18.5,1.4999999 6.838538,0.9769337 13.999999,2.0000003 25,2.0000003 h 25 v 2 h -100 z"
-                />
-            </svg>
-
-
-            <div className="fixed-bottom bg-dark taille-footer">
-
-            </div>
-
-        </>
+        </footer>
 
     )
 }
