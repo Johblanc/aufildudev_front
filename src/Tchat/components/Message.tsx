af6c861aafbb5bb602dec3c7152ad59d021003f0
kevinbatard <kevin.batard4064@gmail.com>
2023-03-28 11:06:43 +0200

    fix message du tchat

--- src/Tchat/components/Message.tsx
+++ src/Tchat/components/Message.tsx
@@ -5,9 +5,9 @@ import { TTchat } from '../types/TTchat';
 export default function Message(props: { message: TTchat }) {
     const userData = useContext(UserContext);
 
-    const BS_COMMUN = 'text-break m-2 pt-2 pb-2 pe-3 ps-3 rounded-4 messages';
+    const BS_COMMUN = 'text-break m-2 pt-2 pb-2 pe-3 ps-3 rounded-4';
     const BS_USER = 'bg-secondary align-self-end';
-    const BS_OTHER = 'bg-info ';
+    const BS_OTHER = 'bg-info align-self-start';
 
     const preventDragHandler = (e: any) => {
         e.preventDefault();
